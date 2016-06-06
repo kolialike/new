25374742ec4aae4520e0e0490bdd0bf2fe146a35
kolialike <kolialike@gmail.com>
2016-06-06 16:10:20 +0300

    commit_

--- src/js/main.js
+++ src/js/main.js
@@ -9,16 +9,16 @@ jQuery(function($){
         slidesPerView: 1,
         centeredSlides: true,
         paginationClickable: true,
-        initialSlide: 1,
+        initialSlide: 1
     });
    
      //tabs
-  var devicesImg = $(".devices-img").find("img");
+  var devicesImg = $(".devices-img-container").find("img");
   var tabs = $(".tabs").find("a");
   tabs.on('click', function(event) {
     event.preventDefault();
     $(this).addClass('active').siblings().removeClass('active');
     var index = tabs.index($(this));
-    devicesImg.eq(index).addClass('active').siblings().removeClass('active');  
+    devicesImg.eq(index).addClass('active').siblings().removeClass('active');
 	});
 });
